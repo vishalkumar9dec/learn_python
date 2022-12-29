0d95230c8c4d9a0c52c2b8593089ee2cd9421415
vishalkumar9dec <60668736+vishalkumar9dec@users.noreply.github.com>
2022-12-29 10:11:58 +0530

    Modules and HOF

---
+++ my_app.py
@@ -0,0 +1,18 @@
+# import test_modules
+
+# name = test_modules.full_name("Alex", "Hill")
+
+# print("fullname : ", name)
+
+
+
+# from test_modules import full_name
+
+# my_name = full_name("Leo", "Cap")
+# print(my_name)
+
+
+
+from test_modules import full_name as fn 
+
+print(fn("Kate", "Winslet"))
